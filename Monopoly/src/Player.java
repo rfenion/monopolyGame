4b42b3a8cb18159ffc6435f475b042b2e89ee1d9
System Administrator <root@Rowans-MacBook-Pro-2.local>
2017-01-22 00:46:34 +0000

    initial commit

---
+++ Monopoly/src/Player.java
@@ -0,0 +1,8 @@
+
+public class Player {
+
+	// instance variable to allow player to lose and gain money.start with 1500 pounds
+	private int balance = 1500;
+	private String piece; 
+	
+}
---
+++ Monopoly/src/Properties.java
@@ -0,0 +1,21 @@
+
+public class Properties {
+	private String name = "";
+	private int buyValue;
+	private String PropertyType;
+	private int rentValue; //with house count or station count at what?
+	private int mortgageValue;
+	private int houseValue;
+	private int hotelValue; 
+	
+	
+	
+	
+	
+	
+	
+	
+	//need and array of prop names, and array of chance&communitychest posibilities
+	//everything has a rent, apart from utilities. set each square on the board to an index in an array, set index x and y (the electric company and the water works) to speacial circs,  
+	//put all the info about a prop in its index value and call it when its landed on, or write a long if statment checking conditions (is it a station... do x, is it a house... do y etc)
+}
